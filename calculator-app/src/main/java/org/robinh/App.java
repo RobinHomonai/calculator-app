4f920ac45fe22d5e66326f4caba1807d896f2125
Robin Homonai <159055771+RobinHomonai@users.noreply.github.com>
2024-02-29 11:38:45 +0100

    Add switch cases with error handling

--- calculator-app/src/main/java/org/robinh/App.java
+++ calculator-app/src/main/java/org/robinh/App.java
@@ -5,7 +5,36 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        menu();
+
+        do {
+            menu();
+            if (scanner.hasNextInt()) {
+                int userInput = scanner.nextInt();
+
+                switch (userInput) {
+                    case 1:
+                        calculation("Addition", scanner);
+                        break;
+                    case 2:
+                        calculation("Subtraction", scanner);
+                        break;
+                    case 3:
+                        calculation("Multiplication", scanner);
+                        break;
+                    case 4:
+                        calculation("Division", scanner);
+                        break;
+                    case 5:
+                        System.out.println("See you next time and have a good day!");
+                        return;
+                    default:
+                        System.out.println("Enter one of the numbers above!");
+                }
+            } else {
+                System.out.println("That's not a number!");
+                scanner.next();
+            }
+        } while (true);
     }
 
     public static void menu() {
